1d16de0337acf7a374e67ba6d1b87345c227b0c9
Nitin-Raj21 <73432121+Nitin-Raj21@users.noreply.github.com>
2024-02-16 14:53:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let redBtn = document.getElementById("red");
+let blueBtn = document.getElementById("blue");
+let blackBtn = document.getElementById("black");
+let bike = document.getElementById("bike");
+
+redBtn.onclick = function(){
+    bike.style.backgroundImage = "url(D:/Nitin/Dropbox/PC/Desktop/bike/BMW1.png)";
+}
+
+blueBtn.onclick = function(){
+    bike.style.backgroundImage = "url(D:/Nitin/Dropbox/PC/Desktop/bike/BMW2.png)";
+}
+
+blackBtn.onclick = function(){
+    bike.style.backgroundImage = "url(D:/Nitin/Dropbox/PC/Desktop/bike/BMW3.png)";
+}
